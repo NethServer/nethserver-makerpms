c6920135fa8957ab20a17eb8c7b4d3f94b1063c3
Davide Principi <davide.principi@nethesis.it>
2020-06-25 15:28:22 +0200

    Create a devtoolset image
    The devtoolset SCL family ships modern versions of the GCC compiler
    and other development tools required to compile some packages.

--- travis/build-container.sh
+++ travis/build-container.sh
@@ -8,8 +8,13 @@ if [[ "${NSLATEST}" == "${NSVER}" ]]; then
     docker tag ${DOCKER_IMAGE} nethserver/makerpms:latest
     docker push nethserver/makerpms:latest
 fi
-docker run -ti --name buildsys ${DOCKER_IMAGE} sudo yum install -y @buildsys-build
+docker run -ti --name buildsys ${DOCKER_IMAGE} sudo yum --setopt=tsflags=nodocs install -y @buildsys-build
 docker commit buildsys nethserver/makerpms:buildsys${NSVER}
 docker push nethserver/makerpms:buildsys${NSVER}
+if [[ "${NSVER}" == 7 ]]; then
+    docker run -ti --name devtoolset nethserver/makerpms:buildsys${NSVER} sudo yum --setopt=tsflags=nodocs install --enablerepo=ce-sclo-rh -y devtoolset-9-\*
+    docker commit devtoolset nethserver/makerpms:devtoolset${NSVER}
+    docker push nethserver/makerpms:devtoolset${NSVER}
+fi
 docker logout
 popd
